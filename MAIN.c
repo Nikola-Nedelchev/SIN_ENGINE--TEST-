07bebd4e742fa9cb65fa4d30866b393fa73efd75
Nikola-Nedelchev <nicola123asd4@gmail.com>
2024-10-08 12:28:06 +0300

    Update MAIN.c

--- MAIN.c
+++ MAIN.c
@@ -45,31 +45,33 @@ int InitWindow(){
 
 int InitVulkan()
 {
-	CreateInstanse();//line
+CreateInstanse();//line
 }
 
 int CreateInstanse() 
-		{
-		VkApplicationInfo appInfo{};
-		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-		appInfo.pApplicationName = "SIN ENGINE";
-		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-		appInfo.pEngineName = "SIN ENGINE";
-		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-		appInfo.apiVersion = VK_API_VERSION_1_0;
+{
 
-		VkInstanceCreateInfo createInfo{};
-		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-		createInfo.pApplicationInfo = &appInfo;
+			
+	VkApplicationInfo appInfo{};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "SIN ENGINE";
+	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.pEngineName = "SIN ENGINE";
+	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.apiVersion = VK_API_VERSION_1_0;
 
-		uint32_t glfwExtensionCount = 0;
-		const char** glfwExtensions;
-		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+	VkInstanceCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
 
-		createInfo.enabledExtensionCount = glfwExtensionCount;
-		createInfo.ppEnabledExtensionNames = glfwExtensions;
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
-		createInfo.enabledLayerCount = 0;
+	createInfo.enabledExtensionCount = glfwExtensionCount;
+	createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+	createInfo.enabledLayerCount = 0;
 
 		#ifdef DEBUG
 		if (vkCreateInstance(&createInfo, NULL, &instance) != VK_SUCCESS) {
@@ -81,8 +83,8 @@ int CreateInstanse()
 
 
 
-int SceneLoop() {
-
+int SceneLoop() 
+{
 	while (!glfwWindowShouldClose(window)) 
 	{
 		glfwPollEvents();
